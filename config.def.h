dc8dec0de3ad37bba1cc3c20406b87c22a86135b
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-08-12 09:52:40 +0400

    font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,13 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Mono:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Symbola:pixelsize=15:antialias=true:autohint=true" // Unicode
+};
+
 static int borderpx = 2;
 
 /*
