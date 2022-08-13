d7e0b7b0f1a4d698e3b3969852378959ca1c5eb0
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-08-13 11:53:07 +0400

    use NotoColorEmoji

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static char *font = "Mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = {
 /*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-	"Symbola:pixelsize=15:antialias=true:autohint=true" // Unicode
+	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" // Unicode
 };
 
 static int borderpx = 2;
